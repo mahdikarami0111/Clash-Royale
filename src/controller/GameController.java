f3bbfaa23464a0d897fd0af17141f906416ea5af
Abtin Angoorani <9931093@ceit.local>
2021-07-21 15:55:13 +0430

    almost done(3)

---
+++ src/controller/GameController.java
@@ -0,0 +1,11 @@
+package controller;
+
+import model.enums.Type;
+
+import java.util.ArrayList;
+
+public class GameController {
+    public void setDeck(ArrayList<Type> cards){}
+    public void setGameManager(int lvl, int botDifficulty){}
+    public void setView(){}
+}
